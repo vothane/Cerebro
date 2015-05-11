85eb8a77f2e6ef6d4200b806769e344b853dc5d0
vothane <vothane@gmail.com>
2015-05-11 07:11:19 +0000

    update weights correctly in train function for logistic regression

--- src/cerebro/LogisticRegression/LogReg.clj
+++ src/cerebro/LogisticRegression/LogReg.clj
@@ -15,7 +15,7 @@
   (->LogReg n 
             n-in 
             n-out 
-            (partition n-out (take (* n-out n-in) (repeat 0.0))) 
+            (mapv vec (partition n-out (take (* n-out n-in) (repeat 0.0)))) 
             (take n-out (repeat 0.0))))	           
 
 (defn softmax [x-coll]
@@ -27,6 +27,11 @@
   (let [mults (map #(map (fn [a b] (* a b)) x %) (:weights logreg))
         px|y  (map #(reduce + %) mults)
         px|y  (map #(+ %1 %2) px|y (:bias logreg))
-        px|y  (softmax px|y)]
-    px|y))
+        px|y  (softmax px|y)
+        f     (fn [i j] (* lr (- (nth y i) (nth px|y i)) (/ (nth y j) (:N logreg))))
+        lg    (reduce 
+                #(update-in %1 [:weights (nth %2 0) (nth %2 1)] + (f (nth %2 0) (nth %2 1))) 
+                logreg
+                (for [i (range (:num-outputs logreg)) j (range (:num-outputs logreg))] [i j]))]
+    lg))
 
