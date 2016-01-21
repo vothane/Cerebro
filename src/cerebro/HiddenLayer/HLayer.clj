9ee81831b89b76d6dbacdf0a3401e01a67064438
Thane Vo <vothane@gmail.com>
2016-01-21 00:01:56 +0000

    refactor sample-h-given-v function

--- src/cerebro/HiddenLayer/HLayer.clj
+++ src/cerebro/HiddenLayer/HLayer.clj
@@ -7,5 +7,4 @@
     (sigmoid linear-output)))
 
 (defn sample-h-given-v [hidden-layer biases input]
-  (let [n-outputs (count (first hidden-layer))]
-    (map #(binomial 1 (output hidden-layer input %1 %2)) hidden-layer biases)))
+  (map #(binomial 1 (output hidden-layer input %1 %2)) hidden-layer biases))
