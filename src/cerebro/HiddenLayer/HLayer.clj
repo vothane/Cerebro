307deb1d10b5340a40efb3509c789d9f99f0b9f0
Thane Vo <vothane@gmail.com>
2016-02-12 02:45:45 +0000

    refactor hidden layer sampling

--- src/cerebro/HiddenLayer/HLayer.clj
+++ src/cerebro/HiddenLayer/HLayer.clj
@@ -6,5 +6,6 @@
         linear-output (+ linear-output bias)]
     (sigmoid linear-output)))
 
-(defn sample-h-given-v [hidden-layer biases inputs]
-  (mapv #(binomial 1 (output inputs %1 %2)) hidden-layer biases))
+(defn hidden-sample-h|v [hidden-layer inputs]
+  (let [{weights :W bias :bias} hidden-layer]
+    (mapv (fn [w_i b_i] (binomial 1 (output inputs w_i b_i))) weights bias)))
