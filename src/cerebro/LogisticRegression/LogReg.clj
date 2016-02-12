caff7f929c93286e3e58bd1a93de6bb65d7954ee
Thane Vo <vothane@gmail.com>
2016-02-12 01:55:39 +0000

    refactor train fn in logistic regression

--- src/cerebro/LogisticRegression/LogReg.clj
+++ src/cerebro/LogisticRegression/LogReg.clj
@@ -8,7 +8,22 @@
 
 (defn softmax [x]
   (let [m (apply max x)
-        x (map #(Math/exp (- % max)) x)
+        x (map #(Math/exp (- % m)) x)
         s (reduce + x)]
     (map #(/ % s) x)))
 
+(defn train [logreg x y lr]
+    (let [{weights :W bias :bias n :N} logreg
+          p-x|y (->> (map #(reduce + (map * x %)) weights)
+                     (map + bias)
+                     (softmax))
+          dy    (map - y p-x|y)
+          _ (println dy)
+          w     (mapv
+                  (fn [w_i dy_i] 
+                    (mapv
+                      (fn [w_ij x_j] (+ w_ij (* lr dy_i (/ x_j n))))
+                      w_i x))
+                  weights dy)
+          b     (mapv (fn [bias_i dy_i] (+ bias_i (* lr (/ dy_i n)))) bias dy)]
+      (assoc logreg :W w :bias b)))
