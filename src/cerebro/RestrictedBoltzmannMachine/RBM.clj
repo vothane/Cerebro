130791f938233b1f785d47645940c59167bbd06f
Thane Vo <vothane@gmail.com>
2016-12-16 17:07:12 -0700

    model RBM domain with a 'closure'

--- src/cerebro/RestrictedBoltzmannMachine/RBM.clj
+++ src/cerebro/RestrictedBoltzmannMachine/RBM.clj
@@ -5,46 +5,52 @@
 ; contain hidden variables. Restricted Boltzmann Machines further restrict BMs
 ; to those without visible-visible and hidden-hidden connections.
 
-(defn RBM-propup [v weights bias]
-  (let [pre-sigmoid-activation (reduce + (map #(* %1 %2) v weights))
-        pre-sigmoid-activation (+ pre-sigmoid-activation bias)]
-    (sigmoid pre-sigmoid-activation)))
+(defn RBM [w hb vb n]
+  {:weights w
+   :hbias hb 
+   :vbias vb
+   :vbias
+   :N n
+   :contrastive-divergence (fn [inputs lr k]
+                             (let [{ph-mean :means ph-sample :samples} (RBM-sample-h-given-v :hbias :weights inputs)
+                                   {{nv-mean :means nv-sample :samples} :v|h
+                                    {nh-mean :means nh-sample :samples} :h|v} (reduce 
+                                                                                  (fn [{{nh-sample :samples} :h|v} _] 
+                                                                                    (RBM-gibbs-hvh hbias vbias W nh-sample)) 
+                                                                                  (RBM-gibbs-hvh hbias vbias W ph-sample) (range k))
+                                   weights (mapv
+                                             (fn [ph-mean_i nh-mean_i W_i] 
+                                               (mapv
+                                                 (fn [W_ij nv-sample_j inputs_j] (+ W_ij (/ (* lr (- (* ph-mean_i inputs_j) (* nh-mean_i nv-sample_j))) n)))
+                                                 W_i nv-sample inputs))
+                                             ph-mean nh-mean W)
+                                   hbias (mapv #(+ (/ (* lr (- %1 %2)) n) %3) ph-sample nh-mean hbias)
+                                   vbias (mapv #(+ (/ (* lr (- %1 %2)) n) %3) inputs nv-sample vbias)]
+                               (RBM weights hbias vbias :N)))
+  })
 
-(defn RBM-propdown [h W bias idx]
-  (let [weights                (nth (matrix-transpose W) idx) 
-        pre-sigmoid-activation (reduce + (map #(* %1 %2) h weights)) 
-        pre-sigmoid-activation (+ pre-sigmoid-activation bias)]
-    (sigmoid pre-sigmoid-activation)))
-
-(defn RBM-sample-h-given-v [hbias W v0-sample]
-  (let [m (mapv #(RBM-propup v0-sample %1 %2) W hbias)
-        s (mapv #(binomial 1 %) m)]
-    {:means m :samples s}))
-
-(defn RBM-sample-v-given-h [vbias W h0-sample]
-  (let [m (map-indexed #(RBM-propdown h0-sample W %2 %1) vbias)
-        s (mapv #(binomial 1 %) m)]
-    {:means (vec m) :samples s}))
-
-(defn RBM-gibbs-hvh [hbias vbias W h0-sample]
-  (let [s-v|h (RBM-sample-v-given-h vbias W h0-sample)]
-    {:v|h s-v|h :h|v (RBM-sample-h-given-v hbias W (:samples s-v|h))}))
-
-(defn RBM-contrastive-divergence [rbm inputs lr k]
-  (let [{hbias :hbias vbias :vbias W :weights n :N} rbm
-        {ph-mean :means ph-sample :samples} (RBM-sample-h-given-v hbias W inputs)
-        {{nv-mean :means nv-sample :samples} :v|h
-         {nh-mean :means nh-sample :samples} :h|v} (reduce 
-                                                       (fn [{{nh-sample :samples} :h|v} _] 
-                                                         (RBM-gibbs-hvh hbias vbias W nh-sample)) 
-                                                       (RBM-gibbs-hvh hbias vbias W ph-sample) (range k))
-        weights (mapv
-                  (fn [ph-mean_i nh-mean_i W_i] 
-                    (mapv
-                      (fn [W_ij nv-sample_j inputs_j] (+ W_ij (/ (* lr (- (* ph-mean_i inputs_j) (* nh-mean_i nv-sample_j))) n)))
-                      W_i nv-sample inputs))
-                  ph-mean nh-mean W)
-        hbias (mapv #(+ (/ (* lr (- %1 %2)) n) %3) ph-sample nh-mean hbias)
-        vbias (mapv #(+ (/ (* lr (- %1 %2)) n) %3) inputs nv-sample vbias)]
-    (assoc rbm :weights weights :hbias hbias :vbias vbias)))
-
+    ;; RBM helper functions
+    (defn RBM-propup [v weights bias]
+      (let [pre-sigmoid-activation (reduce + (map #(* %1 %2) v weights))
+            pre-sigmoid-activation (+ pre-sigmoid-activation bias)]
+        (sigmoid pre-sigmoid-activation)))
+    
+    (defn RBM-propdown [h W bias idx]
+      (let [weights                (nth (matrix-transpose W) idx) 
+            pre-sigmoid-activation (reduce + (map #(* %1 %2) h weights)) 
+            pre-sigmoid-activation (+ pre-sigmoid-activation bias)]
+        (sigmoid pre-sigmoid-activation)))
+    
+    (defn RBM-sample-h-given-v [hbias W v0-sample]
+      (let [m (mapv #(RBM-propup v0-sample %1 %2) W hbias)
+            s (mapv #(binomial 1 %) m)]
+        {:means m :samples s}))
+    
+    (defn RBM-sample-v-given-h [vbias W h0-sample]
+      (let [m (map-indexed #(RBM-propdown h0-sample W %2 %1) vbias)
+            s (mapv #(binomial 1 %) m)]
+        {:means (vec m) :samples s}))
+    
+    (defn RBM-gibbs-hvh [hbias vbias W h0-sample]
+      (let [s-v|h (RBM-sample-v-given-h vbias W h0-sample)]
+        {:v|h s-v|h :h|v (RBM-sample-h-given-v hbias W (:samples s-v|h))}))
